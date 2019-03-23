b9a32aa9ea6191bc85a2e5ec5f84847ac8671a80
felipeweb <fpo@felipeweb.dev>
2019-03-23 12:45:31 -0300

    fix configs

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,52 +1,114 @@
-#
 # Cookbook:: chef-study
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-  execute "update-upgrade" do
-      command "sudo apt-get update && sudo apt-get upgrade -y"
-      action :run
-  end
-  package "apache2" do
-    action :install
-  end
+execute "update-upgrade" do
+    command "sudo apt-get update && sudo apt-get upgrade -y"
+    action :run
+end
+package "apache2" do
+  action :install
+end
 
-  service "apache2" do
-    action [:enable, :start]
-  end
+service "apache2" do
+  action [:enable, :start]
+end
 
-  node["chef_study"]["sites"].each do |sitename, data|
-    
-    document_root = "/var/www/#{sitename}"
-    directory document_root do
-        mode "0755"
-        recursive true
-    end
-    execute "enable-sites" do
-        command "a2ensite 000-#{sitename}"
-        action :nothing
-    end
-    template "/etc/apache2/sites-available/000-#{sitename}.conf" do
-        source "virtualhosts.erb"
-        mode "0644"
-        variables(
-        :document_root => document_root,
-        :servername => data["servername"]
-        )
-        notifies :run, "execute[enable-sites]"
-        notifies :restart, "service[apache2]"
-    end
-    directory "/var/www/#{sitename}/public_html" do
-        action :create
-    end
-    directory "/var/www/#{sitename}/logs" do
-        action :create
-    end
-    template "/var/www/#{sitename}/public_html/index.html" do
-        source "page.erb"
-        mode "0644"
-        variables(
-        :name => data["name"]
-        )
-    end
-  end
+document_root_1 = "/var/www/dev"
+directory document_root_1 do
+    mode "0755"
+    recursive true
+end
+execute "enable-sites-dev" do
+    command "a2ensite 000-dev"
+    action :nothing
+end
+template "/etc/apache2/sites-available/000-dev.conf" do
+    source "virtualhosts.erb"
+    mode "0644"
+    variables(
+    :document_root => document_root_1,
+    :servername => "www.desenvolvimento.com"
+    )
+    notifies :run, "execute[enable-sites-dev]"
+    notifies :restart, "service[apache2]"
+end
+directory "/var/www/dev/public_html" do
+    action :create
+end
+directory "/var/www/dev/logs" do
+    action :create
+end
+template "/var/www/dev/public_html/index.html" do
+    source "page.erb"
+    mode "0644"
+    variables(
+    :name => "Desenvolvimento"
+    )
+end
+
+document_root_2 = "/var/www/qa"
+directory document_root_2 do
+    mode "0755"
+    recursive true
+end
+execute "enable-sites-qa" do
+    command "a2ensite 000-qa"
+    action :nothing
+end
+template "/etc/apache2/sites-available/000-qa.conf" do
+    source "virtualhosts.erb"
+    mode "0644"
+    variables(
+    :document_root => document_root_2,
+    :servername => "www.qa.com"
+    )
+    notifies :run, "execute[enable-sites-qa]"
+    notifies :restart, "service[apache2]"
+end
+directory "/var/www/qa/public_html" do
+    action :create
+end
+directory "/var/www/qa/logs" do
+    action :create
+end
+template "/var/www/qa/public_html/index.html" do
+    source "page.erb"
+    mode "0644"
+    variables(
+    :name => "Q&amp;A"
+    )
+end
+
+document_root_3 = "/var/www/prod"
+directory document_root_3 do
+    mode "0755"
+    recursive true
+end
+execute "enable-sites-prod" do
+    command "a2ensite 000-prod"
+    action :nothing
+end
+template "/etc/apache2/sites-available/000-prod.conf" do
+    source "virtualhosts.erb"
+    mode "0644"
+    variables(
+    :document_root => document_root_3,
+    :servername => "www.producao.com"
+    )
+    notifies :run, "execute[enable-sites-prod]"
+    notifies :restart, "service[apache2]"
+end
+directory "/var/www/prod/public_html" do
+    action :create
+end
+directory "/var/www/prod/logs" do
+    action :create
+end
+template "/var/www/prod/public_html/index.html" do
+    source "page.erb"
+    mode "0644"
+    variables(
+    :name => "Produção"
+    )
+end
